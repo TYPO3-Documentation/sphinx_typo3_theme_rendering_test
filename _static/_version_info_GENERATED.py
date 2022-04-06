119efbc8ea6ce177f8daee6d99925d418d97e356
marble <martin.bless@mbless.de>
2022-04-06 09:05:22 +0000

    Deploying to gh-pages from  @ a5e0f63f5c62cd1e3702a3eeb2f7bac23c4d76d8 🚀

--- _static/_version_info_GENERATED.py
+++ _static/_version_info_GENERATED.py
@@ -1,10 +1,10 @@
 version_info = (
 {
-  "build_mtime": "1644938864",
+  "build_mtime": "1649187663",
   "module_name": "sphinx_typo3_theme",
-  "version_scm": "4.6.3.dev57+g1f6a652",
-  "version_scm_build": "g1f6a652",
+  "version_scm": "4.6.3.dev60+gc7d5ec0",
+  "version_scm_build": "gc7d5ec0",
   "version_scm_core": "4.6.3",
-  "version_scm_pre_release": "dev57"
+  "version_scm_pre_release": "dev60"
 }
 )
